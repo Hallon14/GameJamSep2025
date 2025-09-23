19df438cca51b4b6ac171f843355f86bfc888d03
orvegaard <orvegaard@gmail.com>
2025-09-23 13:37:43 +0200

    Added volley input and various tweaks to player and enemies.

---
+++ Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MeleeEnemy : Enemy
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        SwingSword();
+
+
+    }
+
+    public void SwingSword()
+    {
+        Vector2 projectileDirection = (attackTarget.position - transform.position).normalized;
+        Projectile arrow = Instantiate(projectile, (Vector2)transform.position + projectileDirection, Quaternion.identity).GetComponent<Projectile>();
+        arrow.Initialize(projectileDirection, projectileSpeed, projectileLifetime);
+    }
+
+    public void Move()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Enemies/RangedEnemy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class RangedEnemy : Enemy
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        Move();
+        ShootArrow();
+
+
+    }
+
+    public void ShootArrow()
+    {
+        Projectile arrow = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Projectile>();
+        Vector2 projectileDirection = (attackTarget.position - transform.position).normalized;
+        arrow.Initialize(projectileDirection, projectileSpeed, projectileLifetime);
+    }
+
+    public void Move()
+    {
+
+        if ((attackTarget.position - transform.position).sqrMagnitude > (attackRange / 3) * (attackRange / 3))
+        {
+            rb2D.linearVelocity = -(attackTarget.position - transform.position).normalized * movementSpeed;
+        }
+        else if ((attackTarget.position - transform.position).sqrMagnitude > (attackRange / 2) * (attackRange / 2))
+        {
+            rb2D.linearVelocity = (attackTarget.position - transform.position).normalized * movementSpeed;
+        }
+        else
+        {
+            rb2D.linearVelocity = Vector2.zero;
+        }
+
+    }
+
+}
