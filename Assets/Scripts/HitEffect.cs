2be8f6c9bcecc58514089777236ef9a4447dcc60
orvegaard <orvegaard@gmail.com>
2025-09-25 13:26:51 +0200

    Added hitEffects script (not working).

--- Assets/Scripts/HitEffect.cs
+++ Assets/Scripts/HitEffect.cs
@@ -1,16 +1,60 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.RenderGraphModule;
 
 public class HitEffect : MonoBehaviour
 {
+    private SpriteRenderer sprite;
+
+    public float transitionTime = 2f;
+    private float timer;
+    private float lerpTimer;
+    Color color = Color.white;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // sprite = GetComponent<SpriteRenderer>();
+        // timer = transitionTime;
+        // lerpTimer = 1f - ((transitionTime - timer) / transitionTime);
+        // color = Color.white;
+        // StartCoroutine(SpriteColorEffect());
+    }
+
+    public virtual void TriggerEffect()
+    {
+
+    }
+
+    public IEnumerator SpriteColorEffect()
+    {
+
+        float transitionTime = 2f;
+        float timer = transitionTime;
+        float lerpTimer = 1f - ((transitionTime - timer) / transitionTime);
+        Color color = Color.white;
+        do
+        {
+            color = Color.Lerp(color, Color.black, lerpTimer);
+            sprite.color = color;
+            timer -= Time.deltaTime;
+            yield return null;
+
+        } while (timer <= transitionTime);
+        color = Color.white;
+        yield return null;
+    }
+
+    public void ResetTransition()
+    {
+        timer = transitionTime;
+        lerpTimer = 1f - ((transitionTime - timer) / transitionTime);
+        color = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
